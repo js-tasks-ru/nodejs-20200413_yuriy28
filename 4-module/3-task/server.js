57181315916dbff2f52fb847c7b3e5b5c7840e30
Юрий <yuryboyka47@mail.ru>
2020-05-04 17:51:42 +0300

    4-3 solution

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,23 +1,37 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
 
-  switch (req.method) {
-    case 'DELETE':
-
-      break;
-
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
+  try {
+    switch (req.method) {
+      case 'DELETE':
+        if (pathname.match('\/')) {
+          response(res, 400, 'Bad Request');
+        } else if (!fs.existsSync(filepath)) {
+          response(res, 404, 'Not Found');
+        } else {
+          fs.unlinkSync(filepath);
+          response(res, 200, 'OK');
+        }
+        break;
+      default:
+        response(res, 501, 'Not Implemented');
+    }
+  } catch (e) {
+    response(res, 500, 'Server Error');
   }
 });
+
+function response(res, code, msg) {
+  res.statusCode = code;
+  res.end(msg);
+}
 
 module.exports = server;
